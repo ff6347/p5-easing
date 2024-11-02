f9a08eca6e7a4acc005408086480619a710f2e2d
ff6347 <hash@ff6347.email>
2024-11-02 17:11:08 +0100

    test: scafold basic test files

---
+++ test/elastic.test.ts
@@ -0,0 +1,39 @@
+import { test, describe, expect } from "vitest";
+
+describe("elastic", () => {
+	test("should start at 0", async () => {
+		const easing = await import("../src");
+		expect(easing.easeInElastic).toBeDefined();
+		const time = 0;
+		const start = 0;
+		const end = 1;
+		const duration = 1;
+		const expected = 0;
+		const result = easing.easeInElastic(time, start, end, duration);
+		expect(result).toBe(expected);
+	});
+
+	test("should end close to 1", async () => {
+		const easing = await import("../src");
+		expect(easing.easeInElastic).toBeDefined();
+		const time = 1;
+		const start = 0;
+		const end = 1;
+		const duration = 1;
+		const expected = 1;
+		const result = easing.easeInElastic(time, start, end, duration);
+		expect(result).toBeCloseTo(expected, 10);
+	});
+
+	test("should end at 1", async () => {
+		const easing = await import("../src");
+		expect(easing.easeOutElastic).toBeDefined();
+		const time = 1;
+		const start = 0;
+		const end = 1;
+		const duration = 1;
+		const expected = 1;
+		const result = easing.easeOutElastic(time, start, end, duration);
+		expect(result).toBe(expected);
+	});
+});
---
+++ test/quint.test.ts
@@ -0,0 +1,39 @@
+import { test, describe, expect } from "vitest";
+
+describe("quint", () => {
+	test("should start at 0", async () => {
+		const easing = await import("../src");
+		expect(easing.easeInQuint).toBeDefined();
+		const time = 0;
+		const start = 0;
+		const end = 1;
+		const duration = 1;
+		const expected = 0;
+		const result = easing.easeInQuint(time, start, end, duration);
+		expect(result).toBe(expected);
+	});
+
+	test("should end close to 1", async () => {
+		const easing = await import("../src");
+		expect(easing.easeInQuint).toBeDefined();
+		const time = 1;
+		const start = 0;
+		const end = 1;
+		const duration = 1;
+		const expected = 1;
+		const result = easing.easeInQuint(time, start, end, duration);
+		expect(result).toBeCloseTo(expected, 10);
+	});
+
+	test("should end at 1", async () => {
+		const easing = await import("../src");
+		expect(easing.easeOutQuint).toBeDefined();
+		const time = 1;
+		const start = 0;
+		const end = 1;
+		const duration = 1;
+		const expected = 1;
+		const result = easing.easeOutQuint(time, start, end, duration);
+		expect(result).toBe(expected);
+	});
+});
